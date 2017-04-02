567c88ad14c1ea958c6183c944cc7252b2ca5375
Xiaoyang Qian <qianxiaoyang1996@gmail.com>
2017-04-02 08:32:03 -0700

    initial template

--- mainChecklist.php
+++ mainChecklist.php
@@ -1,22 +1,19 @@
-<?php
-$example = 'example';
-$wellArray = array (
-'Arrays are a lot of fun.',
-'Bootstrap is an amazing development tool to use with PHP',
-'With bootstrap you can quickly code and design beautiful websites'
-);
-?>
 <html>
 <head>
-<title>Checklist</title>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+	<title>Motivator Checklist</title>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
 </head>
+
 <body>
-<h1>Short code <?=$example;?></h1>
-<? foreach($wellArray as $well) {
- print "<div class='well'>$well</div>";
-}
-?>
+
 </body>
+	<!-- Topic section -->
+	<div class="jumbotron text-center">
+		<!-- Embedded Styling -->
+		<h1 style="font-size: 40pt">Claremont Motivator</h1>
+		<p>Let's put procrastination to a stop...</p>
+	</div>
+
+	
 </html>
