63e10d4cc5f671a5715b069d55eca714a9fefce2
Xiaoyang Qian <qianxiaoyang1996@gmail.com>
2017-04-07 15:40:21 -0700

    checklist mockup

--- mainChecklist.php
+++ mainChecklist.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title>Motivator Checklist</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
 </head>
@@ -15,5 +16,42 @@
 		<p>Let's put procrastination to a stop...</p>
 	</div>
 
-	
+	<form class="form-horizontal" role="form" method="post">
+	<table class="table">
+	  <thead>
+	    <tr>
+	      <th>Week day</th>
+	      <th>User 1</th>
+	      <th>User 2</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    <tr>
+	      <th scope="row">Monday</th>
+	      <td><input class="form-check-input" type="checkbox" id="user1task2" value="user1task1"> Task Monday</td>
+	      <td><input class="form-check-input" type="checkbox" id="user2task2" value="user1task1"> Task Monday</td>
+	    </tr>
+	    <tr>
+	      <th scope="row">Tuesday</th>
+	      <td><input class="form-check-input" type="checkbox" id="user1task2" value="user1task2"> Task Tuesday</td>
+	      <td><input class="form-check-input" type="checkbox" id="user2task2" value="user1task2"> Task Tuesday</td>
+	    </tr>
+	    <tr>
+	      <th scope="row">Wednesday</th>
+	      <td><input class="form-check-input" type="checkbox" id="user1task3" value="user1task3"> Task Wednesday</td>
+	      <td><input class="form-check-input" type="checkbox" id="user2task3" value="user1task3"> Task Wednesday</td>
+	    </tr>
+	    <tr>
+	      <th scope="row">Thursday</th>
+	      <td><input class="form-check-input" type="checkbox" id="user1task4" value="user1task4"> Task Thursday</td>
+	      <td><input class="form-check-input" type="checkbox" id="user2task4" value="user1task4"> Task Thursday</td>
+	    </tr>
+	    <tr>
+	      <th scope="row">Friday</th>
+	      <td><input class="form-check-input" type="checkbox" id="user1task5" value="user1task4"> Task Friday</td>
+	      <td><input class="form-check-input" type="checkbox" id="user2task5" value="user1task4"> Task Friday</td>
+	    </tr>	   
+	  </tbody>
+	</table>
+	</form>	
 </html>
