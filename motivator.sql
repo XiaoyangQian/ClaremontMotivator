9a3d799a169d20e9ea13551c7e8d50f893bc489b
Xiaoyang Qian <qianxiaoyang1996@gmail.com>
2017-04-08 17:22:50 -0700

    refactored database

--- motivator.sql
+++ motivator.sql
@@ -8,7 +8,9 @@ CREATE TABLE Users (
 	user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	firstname VARCHAR(256) NOT NULL,
 	lastname VARCHAR(256) NOT NULL,
-	school VARCHAR(256) NOT NULL
+	school VARCHAR(256) NOT NULL,
+	partner_id INT UNSIGNED,
+	FOREIGN KEY (partner_id) REFERENCES Users(user_id)
 );
 
 CREATE TABLE Pairs (
@@ -22,18 +24,22 @@ CREATE TABLE Pairs (
 CREATE TABLE Tasks (
 	week DATE NOT NULL,
 	weekday VARCHAR (256) NOT NULL,
-	pair_id INT UNSIGNED NOT NULL,
-	FOREIGN KEY (pair_id) REFERENCES Pairs(pair_id),
-	task1 VARCHAR(1000) ,
-	task2 VARCHAR(1000) ,
-	task1_check BOOLEAN NOT NULL DEFAULT 0,
-	task2_check BOOLEAN NOT NULL DEFAULT 0
+	user_id INT UNSIGNED NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	task VARCHAR(1000) ,
+	task_check BOOLEAN NOT NULL DEFAULT 0
 );
 
 INSERT INTO USERS (firstname, lastname, school) VALUES ('xiaoyang', 'qian', 'CMC');
 INSERT INTO USERS (firstname, lastname, school) VALUES ('xiangyu', 'fu', 'CMC');
+UPDATE USERS SET partner_id = 2 WHERE user_id = 1;
+UPDATE USERS SET partner_id = 1 WHERE user_id = 2;
 INSERT INTO Pairs (user_id1, user_id2) VALUES (1,2);
-INSERT INTO Tasks (week, weekday, pair_id, task1, task2) VALUES (2017-04-09, 'Sun.', 1, 'read', 'homework');
-
+INSERT INTO Tasks (week, weekday, user_id, task) VALUES (2017-04-09, 'Sun.', 1, 'read');
+INSERT INTO Tasks (week, weekday, user_id, task) VALUES (2017-04-09, 'Sun.', 2, 'i-festival');
+INSERT INTO Tasks (week, weekday, user_id, task) VALUES (2017-04-09, 'Mon.', 1, 'underwater basket weaving');
+INSERT INTO Tasks (week, weekday, user_id, task) VALUES (2017-04-09, 'Mon.', 2, 'procrastinating');
+INSERT INTO Tasks (week, weekday, user_id, task) VALUES (2017-04-09, 'Wed.', 1, 'rehersal');
+INSERT INTO Tasks (week, weekday, user_id, task) VALUES (2017-04-09, 'Wed.', 2, 'sky-diving');
 
 
