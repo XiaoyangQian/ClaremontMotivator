311b30b03f3b1fcedf05f8f895f07c325cc4dd8b
Xiangyu Fu <xfu18@cmc.edu>
2017-04-19 22:48:17 -0700

    MVC refactor

---
+++ mainChecklist.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+	<title>Motivator Checklist</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+</head>
+
+<body>
+
+</body>
+	<!-- Topic section -->
+	<div class="jumbotron text-center">
+		<!-- Embedded Styling -->
+		<h1 style="font-size: 40pt">Claremont Motivator</h1>
+		<p>Let's put procrastination to a stop...</p>
+	</div>
+
+	<!-- <form class="form-horizontal" role="form" method="post"> -->
+		<table class="table">
+		  <thead>
+		    <tr>
+		      <th>Weekday</th>
+		      <th>Myself</th>
+		      <th>My partner</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+			<?php
+				include("dbconn.php");
+				$dbc = connect_to_db("motivator");
+				// [TODO] switch to get later
+				$user_id = 1;
+				// Find pair id
+				$partner_select = $dbc->prepare("SELECT partner_id FROM users 
+													WHERE user_id=?");
+				$partner_select->bind_param("i", $user_id);
+				$partner_select->execute();
+				$partner_select->bind_result($partner_id);
+				if ($partner_select->fetch()) {
+					echo 'yay'.$partner_id;
+				} else {
+					echo 'nay';
+				}
+				$partner_select->free_result();
+				$partner_select->close();
+
+				$_SESSION["user_id"] = $user_id;
+				$_SESSION["partner_id"] = $partner_id;
+
+				$task_select =  $dbc->prepare("SELECT t1.weekday, t1.task as task_1, t2.task as task_2
+								FROM 
+									(SELECT * FROM tasks where user_id=?) t1,
+									(SELECT * FROM tasks where user_id=?) t2
+								WHERE 
+									t1.week = t2.week and 
+									t1.weekday = t2.weekday");
+
+				$task_select->bind_param("ii", $user_id, $partner_id);
+				$task_select->execute();
+				$result = $task_select->get_result();
+				if($result){					
+					while( $row = $result->fetch_assoc() ){
+						$task_1 = $row['task_1'];
+						$task_2 = $row['task_2'];
+						$weekday = $row['weekday'];
+						// [TODO] figure out id to properly save later
+						echo '<tr>
+								<th scope="row">'.$weekday.'</th>
+		      					<td><input class="form-check-input" type="checkbox" id="user1task1" value="user1task1">'.$task_1.'</td>
+		      					<td><input class="form-check-input" type="checkbox" id="user1task1" value="user1task1">'.$task_2.'</td>
+							 </tr>';
+					}
+				}
+			?>	   
+		  </tbody>
+		</table>
+	<!-- </form>	 --> 
+</html>
--- routes.php
+++ routes.php
@@ -8,6 +8,9 @@
       case 'checklist':
         require_once('model/task.php');
         $controller = new ChecklistController();
+      case 'partnerInfo':
+        require_once('model/info.php')
+        $controller = new PartnerInfoController();
       break;
     }
     // call the action
@@ -16,7 +19,7 @@
 
   // just a list of the controllers we have and their actions
   // we consider those "allowed" values
-  $controllers = array('checklist' => ['list', 'error']);
+  $controllers = array('checklist' => ['list', 'error'], 'partnerInfo' => ['list']);
 
   // check that the requested controller and action are both allowed
   // if someone tries to access something else he will be redirected to the error action of the pages controller
