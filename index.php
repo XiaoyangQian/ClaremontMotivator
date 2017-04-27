b0fa0482e8bd31d4538a156769a07e3c950eb682
ShengyueLi <sli18@cmc.edu>
2017-04-27 11:41:21 +0800

    refactor routes

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 // entry point for site
 require_once('dbconn/dbconn.php');
+require_once('routes.php');
 
-if (isset($_GET['controller']) && isset($_GET['action'])) {
-    $controller = $_GET['controller'];
-    $action = $_GET['action'];
-} else {
-    $controller = 'partnerInfo';
-    $action = 'ls';
-}
-require_once('view/layout.php');
+$controller = @$_GET['controller'];
+$action = @$_GET['action'];
+ResolveRoute($controller, $action);
+
--- routes.php
+++ routes.php
@@ -19,19 +19,29 @@ function call($controller, $action)
     $controller->{$action}();
 }
 
-// just a list of the controllers we have and their actions
-// we consider those "allowed" values
-$controllers = array('checklist' => ['ls'], 'partnerInfo' => ['ls'], 'match' => ['direct']);
 
-// check that the requested controller and action are both allowed
-// if someone tries to access something else he will be redirected to the error action of the pages controller
-if (array_key_exists($controller, $controllers)) {
-    if (in_array($action, $controllers[$controller])) {
-        call($controller, $action);
+function ResolveRoute($controller, $action)
+{
+
+    if (!$controller && !$action) {
+        // default route
+        $controller = 'partnerInfo';
+        $action = 'ls';
+    }
+
+    // just a list of the controllers we have and their actions
+    // we consider those "allowed" values
+    $controllers = array('checklist' => ['ls'], 'partnerInfo' => ['ls'], 'match' => ['direct']);
+
+    // check that the requested controller and action are both allowed
+    // if someone tries to access something else he will be redirected to the error action of the pages controller
+    if (array_key_exists($controller, $controllers)) {
+        if (in_array($action, $controllers[$controller])) {
+            call($controller, $action);
+        } else {
+            call('checklist', 'error');
+        }
     } else {
         call('checklist', 'error');
     }
-} else {
-    call('checklist', 'error');
 }
-?>
