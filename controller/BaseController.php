44931fb113d8b08647d9aac73288f4360a5eda23
Xiaoyang Qian <qianxiaoyang1996@gmail.com>
2017-05-01 22:06:46 -0700

    fixed virtual machine view path

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -8,45 +8,60 @@ class BaseController
      */
 
     protected $view_params = [];
-    protected $BASE_VIEW_PATH = '/view/base_layout.php';
+    // protected $BASE_VIEW_PATH = '/../view/base_layout.php';
+    protected $BASE_VIEW_PATH;
 
-    protected function setViewParam($param_name, $param_value){
+    function __construct()
+    {
+        $this->BASE_VIEW_PATH = dirname(__FILE__) . '/../view/base_layout.php';
+    }
+    
+    protected function setViewParam($param_name, $param_value)
+    {
         $this->view_params[$param_name] = $param_value;
     }
 
-    public function renderView($template_file_path){
+    public function renderView($template_file_path)
+    {
         // uses global variable hack to make $this->view_params available for templates
         global $view_params;
         $view_params = $this->view_params;
 
-        function view_param($param_name){
+        function view_param($param_name)
+        {
             global $view_params;
             echo @$view_params[$param_name];
         }
 
-        function view_param_raw($param_name){
+        function view_param_raw($param_name)
+        {
             global $view_params;
             return @$view_params[$param_name];
         }
 
-        function get_link($controller, $action){
+        function get_link($controller, $action)
+        {
             return "/?controller=$controller&action=$action";
         }
+
         // render $template_file_path to global variable $content_html
         ob_start();
-        require '/view/' . $template_file_path . '.php';
+        require dirname(__FILE__) . '/../view/' . $template_file_path . '.php';
+        // require '/view/' . $template_file_path . '.php';
         global $content_html;
         $content_html = ob_get_clean();
 
 
         // export for BASE_VIEW to call
-        function render_content(){
+        function render_content()
+        {
             global $content_html;
             echo $content_html;
         }
 
         // flash message, usually success messages
-        function session_message(){
+        function session_message()
+        {
             $message = @$_SESSION['message'];
             if ($message) {
                 echo '<div class="alert alert-info">' . $message . '</div>';
@@ -54,32 +69,40 @@ class BaseController
             }
         }
 
+
         // render and show BASE_VIEW
         require $this->BASE_VIEW_PATH;
         die();
     }
 
-    public function showErrorAndTerminate($error_message){
+    public function showErrorAndTerminate($error_message)
+    {
         $this->setViewParam('title', 'Error');
         $this->setViewParam('error_message', $error_message);
         $this->renderView('message/custom_error');
     }
 
-    protected function requiresLogin(){
+    protected function requiresLogin()
+    {
         // checks log in state, if not logged in, redirect to log in page.
-        if (!@$_SESSION['user']) {
+        if (!@$_SESSION['user_id']) {
             $this->redirect('auth', 'login');
         }
     }
 
-    protected function getCurrentUser(){
+    /**
+     * @return User
+     */
+    protected function getCurrentUser()
+    {
         if (!@$_SESSION['user_id']) {
             return null;
         }
         return User::find($_SESSION['user_id']);
     }
 
-    protected function redirect($controller, $action){
+    protected function redirect($controller, $action)
+    {
         header("Location: /?controller=$controller&action=$action");
         die();
     }
